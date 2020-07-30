0a18925b7f7f180e1191b4553558eacec01ce04f
Connor Blakeney <connorblakeney@yahoo.com>
2020-07-30 13:43:39 -0500

    moved journal files to a journal folder

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
-import { JournalList } from "./journalList.js"
+import {JournalList} from "./journal/JournalList.js"
+import {getEntries} from "./journal/JournalDataProvider.js"
 
 JournalList()
+getEntries()
