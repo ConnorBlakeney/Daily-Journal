19c08303419d5ce390dd5b5d2dc827ad5c2fcd0c
Connor Blakeney <connorblakeney@yahoo.com>
2020-07-30 13:10:22 -0500

    transplanted data from journalDataProvider to api/entries.json

--- src/scripts/journalDataProvider.js
+++ src/scripts/journalDataProvider.js
@@ -6,59 +6,7 @@
  *      the entries for different purposes.
  */
 
-// This is the original data.
-const journal = [
-  {
-    id: 1,
-    date: "07/06/2020",
-    concept: "HTML & CSS, Git/GitHub",
-    entry:
-      "We talked about HTML components and how to make grid layouts with Flexbox in CSS. Followed up by learning to push everything to GitHub and how to operate Git from the command line.",
-    mood: "Ok",
-  },
-
-  {
-    id: 2,
-    date: "07/13/2020",
-    concept: "Group Project 1",
-    entry:
-      "My group and I (Gib Jeffries, Mikayla Swinkels, Moncia Provence, and myself) used the tools taught to us the first week to make a travel brochure website, which can be found on Github.",
-    mood: "Ok",
-  },
-
-  {
-    id: 3,
-    date: "07/20/2020",
-    concept: "Client Side Mastery Book 1",
-    entry:
-      "We dove into javascript and began by learning the process of automating HTML data structures by completing Martin's Aquarium, Modern Farm, Daily Journal, and Dotard and Simbleton.",
-    mood: "Ok",
-  },
-  {
-    id: 4,
-    date: "07/22/2020",
-    concept: "Group Project 2, Automated Country Websites",
-    entry:
-      "Working with the same group as last time (the Hobbits) we used the skills gained in Book 1 to refactor our website and automate the HTML, as well as several quality of life changes.",
-    mood: "Clap along with Pharrell",
-  },
-  {
-    id: 5,
-    date: "07/23/2020",
-    concept: "APIs and Promises",
-    entry:
-      "Steve and Kristen covered the theory and practice of using APIs and fetch requests.",
-    mood: "OK",
-  },
-  {
-    id: 6,
-    date: "07/24/2020",
-    concept: "Lab Day",
-    entry:
-      "Reviewed concecpts taught yesterday, finished up loose-end assignments, and began to work on Truncheons and Flagons optional group project.",
-    mood: "OK",
-  },
-]
+// This is the original data
 
 /*
     You export a function that provides a version of the
