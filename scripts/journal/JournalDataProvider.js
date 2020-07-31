73f40644487c1fc100f7b38e4c8cd83e2b0993d7
Connor Blakeney <connorblakeney@yahoo.com>
2020-07-31 10:51:13 -0500

    changing HTML and importing data with fetch api

---
+++ scripts/journal/JournalDataProvider.js
@@ -0,0 +1,38 @@
+/*
+ *   Journal data provider for Daily Journal application
+ *
+ *      Holds the raw data about each entry and exports
+ *      functions that other modules can use to filter
+ *      the entries for different purposes.
+ */
+
+let journalEntries = []
+
+const eventHub = document.querySelector(".container")
+
+const dispatchStateChangeEvent = () => {
+  eventHub.dispatchEvent(new CustomEvent("journalStateChanged"))
+}
+
+export const saveJournalEntry = (newJournalEntry) => {
+  // Use `fetch` with the POST method to add your entry to your API
+  fetch("http://localhost:8088/entries", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newJournalEntry),
+  })
+    .then(getEntries) // <-- Get all journal entries
+    .then(dispatchStateChangeEvent) // <-- Broadcast the state change event
+}
+
+export const useJournalEntries = () => {
+  const sortedByDate = journalEntries.sort(
+    (currentEntry, nextEntry) =>
+      Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
+  )
+  return sortedByDate
+}
+
+export const slicedJournal = () => journalEntries.slice()
---
+++ scripts/journal/JournalForm.js
@@ -0,0 +1,38 @@
+import {saveJournalEntry} from "./JournalDataProvider.js"
+import {journalList} from "./JournalEntryList.js"
+
+const journalForm = document.querySelector(".journalForm")
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("click", (clickEvent) => {
+  if (clickEvent.target.id === "conceptsCovered") {
+    const journalEntry = document.querySelector("#journal--entry")
+    const journalConcept = document.querySelector("#journal--concept")
+    const journalMood = document.querySelector("#journal--mood")
+
+    const newNote = {
+      entry: journalEntry.value,
+      concept: journalConcept.value,
+      mood: journalMood.value,
+      date: Date.now(),
+    }
+
+    saveJournalEntry(newNote)
+    journalList()
+  }
+})
+
+export const JournalFormComponent = () => {
+  journalForm.innerHTML += `
+        <input type="text" id="journal--concept" placeholder="Concept" />
+        <textarea id="journal--entry" placeholder ="Entry"></textarea>
+            <select name="mood" id="journal--mood">
+                <option value="Happy">Happy</option>
+                <option value="Sad">Sad</option>
+                <option value="Afraid">Afraid</option>
+                <option value="Mad">Mad</option>
+            </select>
+        <button type="button" name="conceptsCovered" id="conceptsCovered">
+        Record Journal Entry
+    </button>`
+}
